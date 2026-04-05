6c85107733dc9418dfe2f108045f9ce046d2ad0f
1wbillingsley-git <1wb1965197116@gmail.com>
2026-04-05 01:46:28 -0400

    Update githubDeploy.js

--- backend/githubDeploy.js
+++ backend/githubDeploy.js
@@ -4,33 +4,26 @@ const { execSync } = require("child_process");
 async function createRepo() {
   const res = await axios.post(
     "https://api.github.com/user/repos",
-    {
-      name: process.env.REPO_NAME,
-      private: false
-    },
-    {
-      headers: {
-        Authorization: `token ${process.env.GITHUB_TOKEN}`
-      }
-    }
+    { name: process.env.REPO_NAME },
+    { headers: { Authorization: `token ${process.env.GITHUB_TOKEN}` } }
   );
 
   return res.data.clone_url;
 }
 
 function pushToGitHub(repoUrl) {
-  execSync("git init", { stdio: "inherit" });
-  execSync("git add .", { stdio: "inherit" });
-  execSync('git commit -m "Auto deploy"', { stdio: "inherit" });
+  execSync("git init");
+  execSync("git add .");
+  execSync('git commit -m "auto deploy"');
 
   const authUrl = repoUrl.replace(
     "https://",
     `https://${process.env.GITHUB_USERNAME}:${process.env.GITHUB_TOKEN}@`
   );
 
-  execSync(`git remote add origin ${authUrl}`, { stdio: "inherit" });
-  execSync("git branch -M main", { stdio: "inherit" });
-  execSync("git push -u origin main", { stdio: "inherit" });
+  execSync(`git remote add origin ${authUrl}`);
+  execSync("git branch -M main");
+  execSync("git push -u origin main");
 }
 
 module.exports = { createRepo, pushToGitHub };
